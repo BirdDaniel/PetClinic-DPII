e9d14d90f4e797414940dc7863ba9eb0acf681f6
augmb <augustomartinez2a@gmail.com>
2020-03-29 15:00:50 +0200

    Tests

--- src/test/java/org/springframework/samples/petclinic/service/EmployeeServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/EmployeeServiceTests.java
@@ -15,6 +15,7 @@ import org.springframework.dao.DataAccessException;
 
 import org.springframework.samples.petclinic.model.Authorities;
 import org.springframework.samples.petclinic.model.Employee;
+import org.springframework.samples.petclinic.model.Request;
 import org.springframework.samples.petclinic.model.User;
 import org.springframework.stereotype.Service;
 
@@ -97,55 +98,5 @@ class EmployeeServiceTests {
 		.isEqualTo(1);
 	
 	}
-/*
-	@Test
-	void shouldFindSingleOwnerWithPet() {
-		Owner owner = this.employeeService.findOwnerById(1);
-		assertThat(owner.getLastName()).startsWith("Franklin");
-		assertThat(owner.getPets().size()).isEqualTo(1);
-		assertThat(owner.getPets().get(0).getType()).isNotNull();
-		assertThat(owner.getPets().get(0).getType().getName()).isEqualTo("cat");
-	}
-/*
-	@Test
-	@Transactional
-	public void shouldInsertOwner() {
-		Collection<Owner> owners = this.employeeService.findOwnerByLastName("Schultz");
-		int found = owners.size();
-
-		Owner owner = new Owner();
-		owner.setFirstName("Sam");
-		owner.setLastName("Schultz");
-		//owner.setAddress("4, Evans Street");
-		//owner.setCity("Wollongong");
-		owner.setTelephone("4444444444");
-                User user=new User();
-                user.setUsername("Sam");
-                user.setPassword("supersecretpassword");
-                user.setEnabled(true);
-                owner.setUser(user);                
-                
-		this.employeeService.saveOwner(owner);
-		assertThat(owner.getId().longValue()).isNotEqualTo(0);
-
-		owners = this.employeeService.findOwnerByLastName("Schultz");
-		assertThat(owners.size()).isEqualTo(found + 1);
-	}
-
-	@Test
-	@Transactional
-	void shouldUpdateOwner() {
-		Owner owner = this.employeeService.findOwnerById(1);
-		String oldLastName = owner.getLastName();
-		String newLastName = oldLastName + "X";
-
-		owner.setLastName(newLastName);
-		this.employeeService.saveOwner(owner);
-
-		// retrieving new name from database
-		owner = this.employeeService.findOwnerById(1);
-		assertThat(owner.getLastName()).isEqualTo(newLastName);
-	}
-*/
 
 }
