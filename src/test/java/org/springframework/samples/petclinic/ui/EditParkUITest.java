1988ab6f034899cc5319ebbf37a99bc4f8b333ae
augmb <augustomartinez2a@gmail.com>
2020-04-30 14:46:06 +0200

    Test de UI parques

---
+++ src/test/java/org/springframework/samples/petclinic/ui/EditParkUITest.java
@@ -0,0 +1,94 @@
+package org.springframework.samples.petclinic.ui;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class EditParkUITest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    String pathToGeckoDriver="C:/Users/Aug/Documents/DevStuff/Geckodriver";
+
+    System.setProperty("webdriver.gecko.driver",
+                        pathToGeckoDriver +
+                        "\\geckodriver.exe");
+
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testEditParkUI() throws Exception {
+    driver.get("http://localhost:8080/");
+    driver.findElement(By.xpath("//a[contains(@href, '/login')]")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("owner1");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("0wn3r");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[6]/a/span[2]")).click();
+    driver.findElement(By.xpath("//table[@id='parksTable']/tbody/tr/td[3]/a")).click();
+    driver.findElement(By.id("name")).click();
+    driver.findElement(By.id("name")).clear();
+    driver.findElement(By.id("name")).sendKeys("Parque");
+    driver.findElement(By.xpath("//body/div")).click();
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    try {
+      assertEquals("1 Parque", driver.findElement(By.xpath("//table[@id='parksTable']/tbody/tr/td")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
